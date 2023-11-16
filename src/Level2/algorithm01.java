6e90a65e2c1db9557da2bbf888b13da776e01260
siyeon <itsiyeon@nate.com>
2023-11-16 23:59:54 +0900

    :fire: 2023 카카오 블라인드 채용 > 택배 배달과 수거하기

---
+++ src/Level2/algorithm01.java
@@ -0,0 +1,27 @@
+package Level2;
+
+/**
+ * 그리드 문제
+ * 최대 용량 만큼 배달 하면서 이동
+ * 다시 빈 트럭 상태 일때 다시 최대 용량 만큼 수거 가능
+ */
+
+public class algorithm01 {
+    public long solution(int cap, int n, int[] deliveries, int[] pickups) {
+        long answer = 0;
+
+        int d = 0;
+        int p = 0;
+        for (int i=n-1;i>=0;i--) {
+            d-=deliveries[i];
+            p-=pickups[i];
+
+            while (d<0 || p<0) {
+                d+=cap;
+                p+=cap;
+                answer += (i+1)*2;
+            }
+        }
+        return answer;
+    }
+}
