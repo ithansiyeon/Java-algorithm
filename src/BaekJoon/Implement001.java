3d9ec1370cffcc6706ce0bd04e259bc9abdb46fb
itsiyeon <itsiyeon@nate.com>
2026-02-08 19:47:54 +0900

    별 찍기 - 1

---
+++ src/BaekJoon/Implement001.java
@@ -0,0 +1,16 @@
+package BaekJoon;
+
+import java.util.Scanner;
+
+public class Implement001 {
+	public static void main(String[] args) {
+		Scanner kb = new Scanner(System.in);
+		int n = kb.nextInt();
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j <= i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
